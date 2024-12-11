1a2d584805915d8394ef8ae90bd3f742bcb18a4d
Shehanaz22 <shehanaz653@gmail.com>
2024-12-11 06:02:03 +0000

    1

--- Practical1.java
+++ Practical1.java
@@ -16,10 +16,10 @@ class Person {
         this.lastName = lastName;
         this.age = age;
     }
-
-    // Method to get full name
+ // Method to get full name
     public String getFullName() {
-        return firstName + " " + lastName; // Add space between first and last name
+     return firstName + " " + lastName; 
+     // Add space between first and last name
     }
 }
 
@@ -30,11 +30,11 @@ public class Practical1 {
         Person person2 = new Person("Alice", "Smith", 25);
 
         // Print full names of both persons
-        System.out.println("Person 1: " + person1.getFullName());
-        System.out.println("Person 2: " + person2.getFullName());
+        System.out.println("Person1: " + person1.getFullName());
+        System.out.println("Person2: " + person2.getFullName());
 
         // Calculate and print the average age
         double averageAge = (person1.age + person2.age) / 2.0;
-        System.out.println("Average Age: " + averageAge);
+        System.out.println("Average age: " + averageAge);
     }
 }
